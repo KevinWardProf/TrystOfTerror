cf7993c367515def5935d67b99099933c3da0185
KevinWardProf <kwardwork1@gmail.com>
2024-04-05 22:22:13 -0400

    Added basic character controller script, joan object in a test scene, and various folders for later use

---
+++ TrystOfTerror/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,22 @@
+public class CharacterAttributes /*: IComparable<CharacterStatistics>*/
+{
+    public int Vitality { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Endurance { get; set; }
+    public int Agility { get; set; }
+    public int Perception { get; set; }
+    public int Intelligence { get; set; }
+    public int Charisma { get; set; }
+    public int Faith { get; set; }
+    public int Luck { get; set; }
+    public int Willpower { get; set; }
+
+
+
+
+    //public int CompareTo(CharacterStatistics other) 
+    //{
+    //    return (other != null) ? 1 : -1;
+    //}
+}
